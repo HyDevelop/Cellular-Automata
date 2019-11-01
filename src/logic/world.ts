55d1ac80e70e860a77670376b715bd0654dff9ac
Hykilpikonna <admin@moecraft.cc>
2019-11-01 19:16:22 -0400

    [+] Create world class

--- src/logic/world.ts
+++ src/logic/world.ts
@@ -7,3 +7,26 @@ interface WorldConfig
     rules: any
     states: any
 }
+
+export default class World
+{
+    // Config
+    config: WorldConfig;
+
+    // Variables
+    grid: number[][];
+
+    /**
+     * Constructor
+     *
+     * @param config Configuration
+     * @constructor
+     */
+    public World(config: WorldConfig)
+    {
+        this.config = config;
+
+        // Make empty grid
+        this.grid = new Array(config.width).fill(0).map(() => new Array(config.height).fill(0));
+    }
+}
