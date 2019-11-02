f7243bcca7050c08d9d6e57624573719ad9e4794
Hykilpikonna <admin@moecraft.cc>
2019-11-01 21:27:20 -0400

    [+] Create methods for checking conditions and rules

--- src/logic/rules.ts
+++ src/logic/rules.ts
@@ -1,3 +1,4 @@
+import {Cell, Status} from "@/logic/world";
 
 export interface Rule
 {
@@ -10,4 +11,54 @@ export interface Condition
 {
     operator: string
     aliveCount: number
+}
+
+export default class Rules
+{
+    /**
+     * Apply a rule
+     *
+     * @param rule
+     * @param nearby
+     */
+    public static apply(rule: Rule, nearby: Cell[][]): boolean
+    {
+        // Check self status
+        if (nearby[1][1].status !== rule.selfStatus) return false;
+
+        // Count alive
+        let aliveCount = nearby.flat().filter(c => c.status.alive).length - 1;
+
+        // Apply conditions
+        for (let condition of rule.conditions)
+        {
+            if (!this.applyCondition(condition, aliveCount))
+            {
+                return false;
+            }
+        }
+
+        // All conditions passed
+        return true;
+    }
+
+    /**
+     * Apply a condition
+     *
+     * @param condition
+     * @param aliveCount
+     */
+    public static applyCondition(condition: Condition, aliveCount: number): boolean
+    {
+        switch (condition.operator)
+        {
+            case '<': return aliveCount < condition.aliveCount;
+            case '==': return aliveCount == condition.aliveCount;
+            case '>': return aliveCount > condition.aliveCount;
+            case '>=': return aliveCount >= condition.aliveCount;
+            case '<=': return aliveCount <= condition.aliveCount;
+        }
+
+        throw Error('Condition unrecognized: ' + condition.operator);
+    }
 }
