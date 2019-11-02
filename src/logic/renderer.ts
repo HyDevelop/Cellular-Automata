f1831d735f5e30ebe8cf21461cdd50cf31612473
Hykilpikonna <admin@moecraft.cc>
2019-11-01 22:14:22 -0400

    [+] Add constructor to renderer

--- src/logic/renderer.ts
+++ src/logic/renderer.ts
@@ -1,5 +1,19 @@
 
 export default class Renderer
 {
+    private canvas: any;
+    private context: any;
+
+    /**
+     * Construct a renderer
+     *
+     * @param canvas
+     * @constructor
+     */
+    public Renderer(canvas: any)
+    {
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d');
+    }
 
 }
