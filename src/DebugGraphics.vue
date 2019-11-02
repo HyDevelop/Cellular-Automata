3e5e1649e8200140988ce07dc66d173b9418e17c
Hykilpikonna <admin@moecraft.cc>
2019-11-02 14:38:01 -0400

    [+] Create debugGraphics

---
+++ src/DebugGraphics.vue
@@ -0,0 +1,95 @@
+<template>
+    <div id="debug-graphics">
+        <div id="canvas-div">
+            <canvas id="world-canvas"></canvas>
+        </div>
+
+        <div id="controls">
+            <el-button type="info" plain @click="updateButton">Update</el-button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import Renderer from '@/logic/renderer';
+    import World from "@/logic/world";
+    import Rules from '@/logic/rules';
+    import Presets from '@/logic/presets';
+
+    @Component
+    export default class DebugGraphics extends Vue
+    {
+        renderer: Renderer;
+        world: World;
+
+        /**
+         * This is called when the page finishes loading
+         */
+        mounted()
+        {
+            // Find canvas and create renderer
+            let canvas = document.querySelector('#world-canvas');
+            this.renderer = new Renderer(canvas);
+
+            // Set frame
+            this.renderer.setFrame(1000, 500);
+
+            // Draw grid
+            this.renderer.drawGrid();
+
+            // Create world
+            this.world = new World(
+            {
+                name: 'Test',
+                width: 1000,
+                height: 500,
+                rules: Rules.conway,
+                presetCells: new Presets({x: 50, y: 25}).R_PENTOMINO,
+                onUpdate: cell => this.renderer.drawCell(cell)
+            })
+        }
+
+        /**
+         * This is called when the update button is clicked
+         */
+        updateButton()
+        {
+            console.log('Updating');
+            this.world.act()
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    #world-canvas
+    {
+        width: 100%;
+        height: 100%;
+
+        // Dark background
+        background: #000000;
+
+        // Rounded corners
+        border-radius: 10px;
+    }
+
+    #canvas-div
+    {
+        display: flex;
+        box-shadow: 0 2px 12px 0 rgba(0,0,0,.5);
+
+        // Alignment
+        margin: 10px;
+        max-width: 1200px;
+
+        // Rounded corners
+        border-radius: 10px;
+    }
+
+    #controls
+    {
+        // Alignment
+        margin: 30px 10px 10px;
+    }
+</style>
