c8c321016ee06f5a57ffea6b40333bec29f061d9
Hykilpikonna <admin@moecraft.cc>
2019-11-02 11:14:05 -0400

    [S] Remove top margin

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,18 @@ import {Component, Vue} from 'vue-property-decorator';
 import Graphics from '@/Graphics.vue';
 
 @Component({
-  components: {
-    Graphics,
-  },
+  components: {Graphics},
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app
+{
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
