8b23ac8dd682199333f1c585ee162db4e6473f19
Hykilpikonna <admin@moecraft.cc>
2019-11-02 13:30:46 -0400

    [+] Construct presets with center point

--- src/logic/presets.ts
+++ src/logic/presets.ts
@@ -1,4 +1,14 @@
+import {Cell, Point} from '@/logic/world';
+
 export default class Presets
 {
+    x: number;
+    y: number;
+
+    constructor(center: Point)
+    {
+        this.x = center.x;
+        this.y = center.y;
+    }
 
 }
