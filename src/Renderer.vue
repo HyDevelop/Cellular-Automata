dd389da2cb809bd155a26f654b2e29e7b7066624
Hykilpikonna <admin@moecraft.cc>
2019-11-01 21:52:59 -0400

    [+] Create renderer

---
+++ src/Renderer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="renderer">
+        <canvas id="world-canvas"></canvas>
+    </div>
+</template>
+
+<script lang="ts">
+    export default class Renderer
+    {
+        
+    }
+</script>
+
+<style scoped>
+    #world-canvas
+    {
+        width: 100%;
+        height: 100%;
+    }
+</style>
